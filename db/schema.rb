8449648bce76ce62f8547b140e2de4a408023f02
Ryan Travitz <rtravitz@gmail.com>
2016-11-29 14:28:13 -0700

    Pausing on unit price decisions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129205018) do
+ActiveRecord::Schema.define(version: 20161129211011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20161129205018) do
 
   create_table "invoice_items", force: :cascade do |t|
     t.integer  "quantity"
-    t.bigint   "unit_price"
+    t.string   "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "item_id"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20161129205018) do
   create_table "items", force: :cascade do |t|
     t.citext   "name"
     t.text     "description"
-    t.bigint   "unit_price"
+    t.string   "unit_price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "merchant_id"
