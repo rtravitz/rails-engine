c89a5690f7554c9199e9b5a39f636fb9512e9b75
Daniel Shin <shin.daniel.d@gmail.com>
2016-11-28 21:47:06 -0700

    add merchant model test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,11 +1,25 @@
 require 'rails_helper'
 
-# describe "merchant" do
-#   context "validations" do
-#     it "is invalid without a name" do
-#       merchant = Merchant.create()
-#
-#       expect(merchant).to be_invalid
-#     end
-#   end
-# end
+describe "merchant" do
+  context "validations" do
+    it "is invalid without a name" do
+      merchant = Merchant.new()
+
+      expect(merchant).to be_invalid
+    end
+
+    it "is valid with a name" do
+      merchant = Merchant.new(name: "Test")
+
+      expect(merchant).to be_valid
+    end
+  end
+
+  context "relationships" do
+    it "has many items" do
+      merchant = Merchant.new(name: "Test")
+
+      expect(merchant).to respond_to(:items)
+    end
+  end
+end
