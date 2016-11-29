e5441255554b8d5a7cdf475d12aa03b4b777017f
Ryan Travitz <rtravitz@gmail.com>
2016-11-29 16:33:46 -0700

    Add test for customer invoice relationship

---
+++ spec/requests/api/v1/customers/customers_relationships_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "customers relationships endpoints" do
+  context "GET /api/v1/customers/:id/invoices" do
+    it "returns a list of all invoices for one customer" do
+      customer = create(:customer)
+      create_list(:invoice, 3, customer_id: customer.id)
+
+      get "/api/v1/customers/#{customer.id}/invoices"
+
+      data = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(data.count).to eq(3)
+    end
+  end
+end
