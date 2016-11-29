dcb2d0daefd55b33bf0b4c59ce74ec65286e078e
Ryan Travitz <rtravitz@gmail.com>
2016-11-28 23:32:14 -0700

    Add test for invoices find

--- spec/requests/api/v1/invoices/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices/invoices_request_spec.rb
@@ -26,4 +26,18 @@ RSpec.describe "invoices endpoints" do
       expect(data["status"]).to eq(invoice.status)
     end
   end
+
+  context "GET /api/v1/invoices/find" do
+    it "returns an invoice by passed in criteria" do
+      create_list(:invoice, 3)
+      invoice = Invoice.first
+
+      get "/api/v1/invoices/find?status=#{invoice.status}"
+
+      data = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(data["status"]).to eq(invoice.status)
+    end
+  end 
 end
