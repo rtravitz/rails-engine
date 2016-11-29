08b68bf629d657849fcda182476ee00cc31f71f3
Ryan Travitz <rtravitz@gmail.com>
2016-11-28 22:31:20 -0700

    Add show route to items search controller

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,3 +1,12 @@
 class Api::V1::Items::SearchController < ApplicationController
+  def show
+    render json: Item.find_by(item_params)
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price)
+  end
 
 end
