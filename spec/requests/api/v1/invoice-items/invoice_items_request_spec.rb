728c1d87a6165bdd3fa2509e520accdbf95dc270
Ryan Travitz <rtravitz@gmail.com>
2016-11-28 17:57:39 -0700

    Add invoice items index test

---
+++ spec/requests/api/v1/invoice-items/invoice_items_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "invoice items endpoints" do
+  context "GET /api/v1/invoices" do
+    it "returns a list of all invoice_items" do
+      create_list(:invoice_items, 3)
+
+      get "/api/v1/invoice_items"
+
+      invoice_items = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(invoices.count).to eq(3)
+    end
+  end
+end
