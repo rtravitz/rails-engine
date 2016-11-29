d101ed828ec6f8f203c067d3bbe7b0d1436bf128
Ryan Travitz <rtravitz@gmail.com>
2016-11-28 22:54:59 -0700

    Pass find all test for items

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::Items::SearchController < ApplicationController
+  def index
+    render json: Item.where(item_params)
+  end
+
   def show
     render json: Item.find_by(item_params)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
       namespace :items do
         get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
       end
       
       resources :items, only: [:index, :show]
