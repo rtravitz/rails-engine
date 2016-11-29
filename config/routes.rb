90565bb1c45e8d47e5c239bdbcccd2862a08058f
Daniel Shin <shin.daniel.d@gmail.com>
2016-11-28 23:30:53 -0700

    add search controller and find method

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,13 @@ Rails.application.routes.draw do
         get 'find_all', to: 'search#index'
         get 'random', to: 'random#show'
       end
-      
+
+      namespace :customers do
+        get 'find', to: 'search#show'
+        get 'find_all', to: 'search#index'
+        get 'random', to: 'random#show'
+      end
+
       resources :items, only: [:index, :show]
       resources :merchants, only: [:index, :show]
       resources :transactions, only: [:index, :show]
