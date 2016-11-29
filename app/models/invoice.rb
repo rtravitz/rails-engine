84eebb35a805f6af9a7e6acbc237c22fd915a4f6
Daniel Shin <shin.daniel.d@gmail.com>
2016-11-29 10:52:19 -0700

    cleanup invoices, items, merchants random method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,10 +4,4 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
 
   validates :status, presence: true
-
-  def self.random
-    offset = rand(Invoice.count)
-    rand_record = Invoice.offset(offset).first
-  end
-
 end
