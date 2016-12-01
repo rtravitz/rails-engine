ec19e0380d71edeee033525e64a2e18aed618b13
Ryan Travitz <rtravitz@gmail.com>
2016-11-30 17:33:17 -0700

    Pass spec harness

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,14 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   validates :name, :description, :unit_price, presence: true
+
+  def self.most_revenue(quantity = 5)
+    Item.select("items.*, sum(invoice_items.quantity) * items.unit_price as total_price")
+          .joins(:invoice_items)
+          .joins(invoices: :transactions)
+          .merge(Transaction.successful)
+          .group(:id)
+          .order("total_price DESC")
+          .limit(quantity)
+  end
 end
---
+++ spec/requests/api/v1/items/items_business_intelligence_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "items business intelligence endpoints" do
+  context "GET /api/v1/items/:id/most_revenue?quantity=x" do
+    it "returns the top x items ranked by total revenue generated" do
+       
+    end
+  end
+end
