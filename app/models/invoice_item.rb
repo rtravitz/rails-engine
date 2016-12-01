b5cac924848b992da0d99314a8caf9b7dddf7fc5
Ryan Travitz <rtravitz@gmail.com>
2016-12-01 15:24:50 -0700

    Remove random method from invoice item

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,8 +2,4 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
   validates :quantity, :unit_price, presence: true
-
-  def self.random
-    order("RANDOM()").first
-  end
 end
