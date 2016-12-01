5f66ecdb23e4277e07718b0aae5684dc22f75489
Daniel Shin <shin.daniel.d@gmail.com>
2016-12-01 13:26:05 -0700

    add best day serializer

--- app/controllers/api/v1/items/best_day_controller.rb
+++ app/controllers/api/v1/items/best_day_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Items::BestDayController < ApplicationController
   def show
-    render json: Item.find(params[:id]).best_day
+    render json: Item.find(params[:id]).best_day, serializer: BestDaySerializer
   end
 end
