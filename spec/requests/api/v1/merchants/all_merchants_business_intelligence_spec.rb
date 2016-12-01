d70a3c1e29e620d53b5bf36f78a10967ec661235
Ryan Travitz <rtravitz@gmail.com>
2016-12-01 10:41:14 -0700

    Remove vestigial test

--- spec/requests/api/v1/merchants/all_merchants_business_intelligence_spec.rb
+++ spec/requests/api/v1/merchants/all_merchants_business_intelligence_spec.rb
@@ -51,9 +51,4 @@ RSpec.describe "all merchants business intelligence endpoints" do
     end
   end
 
-  context "GET /api/v1/merchants/revenue?date=x" do
-    it "returns the total revenue for date x across all merchants" do
-      
-    end
-  end
 end
