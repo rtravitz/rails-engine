0a0679923142be1a2a13478caf63ef965c09d283
Ryan Travitz <rtravitz@gmail.com>
2016-12-01 10:39:25 -0700

    Pass spec harness

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -67,12 +67,12 @@ class Merchant < ApplicationRecord
   end
 
   def self.total_revenue(date)
-    {"total_revenue" => 
-      to_float(joins(invoices: [:invoice_items, :transactions])
-      .merge(Transaction.successful)
-      .where(invoices: {created_at: date})
-      .sum("invoice_items.quantity * invoice_items.unit_price"))
-    }
+    result = joins(invoices: [:invoice_items, :transactions])
+    .merge(Transaction.successful)
+    .where(invoices: {created_at: date})
+    .sum("invoice_items.quantity * invoice_items.unit_price")
+    result = (result.to_f / 100.00).to_s
+    {"total_revenue" => result }
   end
 
 end
