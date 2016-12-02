82acf48cb5b287927489eb1c959633b4b360e4fa
Ryan Travitz <rtravitz@gmail.com>
2016-12-01 17:36:42 -0700

    Add tests for best day and items sold

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -44,4 +44,35 @@ describe "invoice item" do
       expect(invoice_item).to respond_to(:invoice)
     end
   end
+
+  context "methods" do
+    context "best_day" do
+      it "returns the date with the most sales for the given item" do
+        item = create(:item)
+        invoice_1 = create(:invoice, created_at: "2012-03-16 11:55:05")
+        invoice_2 = create(:invoice, created_at: "2013-03-16 11:55:05")
+        create(:invoice_item, item: item, invoice: invoice_1, quantity: 200)
+        create(:invoice_item, item: item, invoice: invoice_1, quantity: 300)
+        create(:invoice_item, item: item, invoice: invoice_2, quantity: 400)
+
+        expect(item.best_day).to eq(invoice_1.created_at)
+      end
+    end 
+
+    context "self.most_items_sold"  do
+      it "returns the top x item instances ranked by total number sold"  do
+        item_1, item_2, item_3 = create_list(:item, 3)
+        invoice = create(:invoice)
+        transaction_1, transaction_2, transaction_3 = create_list(:transaction, 3, invoice: invoice, result: 'success')
+        invoice_item_1 = create(:invoice_item, item: item_1, invoice: invoice, quantity: 200)
+        invoice_item_1 = create(:invoice_item, item: item_2, invoice: invoice, quantity: 300)
+        invoice_item_1 = create(:invoice_item, item: item_3, invoice: invoice, quantity: 400)
+
+        response = Item.most_items_sold(2)
+
+        expect(response.count).to eq(2)
+        expect(response.first.id).to eq(item_3.id)
+      end
+    end
+  end
 end
