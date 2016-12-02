08d14a7236e7d6a933522d44b1703ea8b5707a36
Ryan Travitz <rtravitz@gmail.com>
2016-12-01 18:04:14 -0700

    Add test for most revenue

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -74,5 +74,21 @@ describe "invoice item" do
         expect(response.first.id).to eq(item_3.id)
       end
     end
+
+    context "self.most_revenue" do
+      it "returns the top x items ranked by total revenue generated" do
+        item1, item2 = create_list(:item, 2, unit_price: 1)
+        invoice = create(:invoice)
+        create_list(:transaction, 2, invoice: invoice, result: "success")
+        ii1 = create(:invoice_item, item: item1, invoice: invoice)
+        ii2 = create(:invoice_item, item: item1, invoice: invoice)
+        ii3 = create(:invoice_item, item: item2, invoice: invoice)
+
+        response = Item.most_revenue
+
+        expect(response.length).to eq(2)
+        expect(response.first.id).to eq(item1.id)
+      end
+    end
   end
 end
